c38b7f27289ef3829a969c252f3749c181e07a29
Kimmo Puputti <kimmo.puputti@hexagonit.fi>
2010-06-12 14:59:24 +0200

    Translated functions from the PHP script.

--- src/degrade.py
+++ src/degrade.py
@@ -4,22 +4,56 @@ Usage: python degrade.py file
 
 where file is the name of the AOL data file.
 """
-
-
 from datetime import datetime
+import math
 import sys
 
 
-def totworth(datum):
-    pass
+# Smoothing technique.
+s = 18
+# Weights.
+w = [1, 0.3, 0.2, 0.1]
+# Risk factors.
+r = [1, 0.2, 0.1, 0.05]
+# Datum intervals.
+d = [0, 16, 46, 96, 96]
+# Degradation steps.
+n = 4
+# Number of items in store.
+N = 100
+# Constant c.
+c = 1
+
+
+def wt(a, i):
+    return w[i] * (1 + math.cos((a * math.pi) / 180))
 
 
 def risk(datum):
-    pass
+    sum = 0
+    for l in xrange(n):
+        sum += r[l] * (d[l + 1] - d[l])
+    return c * sum
 
 
 def priv(datum):
-    pass
+    return 1 / (s + risk(datum))
+
+
+def CI(datum):
+    return totworth(datum) * priv(datum)
+
+
+def totworth(datum):
+    tsum = 0
+    for l in xrange(n - 1):
+        inner_sum = 0
+        a = d[l]
+        while a < d[l + 1]:
+            inner_sum += wt(a, l)
+            a += 1
+        tsum += inner_sum
+    return c * tsum
 
 
 def degrade(datum):
@@ -54,7 +88,12 @@ def main(file_name):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        sys.stderr.write('Not enough arguments.\n' + __doc__)
-        sys.exit(2)
-    sys.exit(main(sys.argv[1]))
+    # if len(sys.argv) != 2:
+    #     sys.stderr.write('Not enough arguments.\n' + __doc__)
+    #     sys.exit(2)
+    # sys.exit(main(sys.argv[1]))
+
+    print 'CI(d):', CI(d)
+    print 'totworth(d):', totworth(d)
+    print 'priv(d):', priv(d)
+    print 'risk(d):', risk(d)
