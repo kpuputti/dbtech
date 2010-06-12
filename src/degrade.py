870e38c2f8ccb25a36cf70a7b9e1514823b1a44a
Kimmo Puputti <kimmo.puputti@hexagonit.fi>
2010-06-12 18:23:56 +0200

    Refactored script structure and added graph generation.

--- src/degrade.py
+++ src/degrade.py
@@ -1,10 +1,17 @@
 from datetime import datetime
+from matplotlib import pyplot as plt
 import math
+import operator
 import sys
 
 
 # Path to the file that hold the data.
 DATA_FILE = 'data/user.txt'
+
+
+# Value to apply to calculations to escape mathematical problems and
+# to provide a more realistic model of privacy.
+SMOOTHING_FACTOR = 18
 
 
 def get_data():
@@ -22,61 +29,111 @@ def get_data():
     return data
 
 
-def wt(weight, timedelta):
+def weight(age):
+    """The weight factor for the given age."""
+    days = age.days
+    if days < 21:
+        # Under 3 weeks old.
+        return 1
+    elif days < 42:
+        # 3 to 6 weeks old.
+        return 0.3
+    elif days < 63:
+        # 6 to 9 weeks old.
+        return 0.2
+    else:
+        # Over 9 weeks old.
+        return 0.1
+
+
+def wt(age):
     """Worth of one datum to the service provider."""
-    return weight * (1 + math.cos((timedelta.days * math.pi) / 180))
+    return weight(age) * (1 + math.cos((age.days * math.pi) / 180))
 
 
-def totworth(data, timestamp, weight):
+def totworth(data, timestamp):
     """Total worth of the data to the service provider."""
-    return sum(wt(weight, timestamp - datum[2]) for datum in data)
+    return sum(wt(timestamp - datum[2]) for datum in data)
 
 
 def risk(data):
+    """Risk factor of the data for the user."""
     return len(data)
 
 
-def priv(data, smoothing_factor):
-    return 1 / (smoothing_factor * risk(data))
+def priv(data):
+    """Privacy guarantee of the data to the user."""
+    return 1 / (SMOOTHING_FACTOR * risk(data))
 
 
-def CI(data):
-    pass
+def CI(data, timestamp):
+    """Common interest count for the data."""
+    return totworth(data, timestamp) * priv(data, SMOOTHING_FACTOR)
 
 
-def stats(data, timestamp, degradation_steps, weight, risk_factor):
-    print '\nDegradation steps:', degradation_steps
-    print 'weight:', weight
+def print_stats(data, timestamp, degradation_steps, risk_factor):
+    print 'Degradation steps:', degradation_steps
     print 'risk factor:', risk_factor
-    print 'total worth:', totworth(data, timestamp, weight)
+    print 'total worth:', totworth(data, timestamp)
+    print ''
 
 
-def main(args):
-    smoothing_factor = 18
+def graph(values, title, xlabel, ylabel, file_name):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot([x for x, y in values], [y for x, y in values], 'o-')
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    fig.savefig('output/%s' % file_name)
+
+
+def generate_graphs(data, timestamp, mindate, maxdate):
+    weights = []
+    for datum in data:
+        age = timestamp - datum[2]
+        dweight = weight(age)
+        weights.append((age.days, dweight))
+    graph(weights, 'weights', 'age', 'weight', 'weights.png')
+
+def main(operation):
     weights = (1, 0.3, 0.2, 0.1)
     risk_factors = (1, 0.2, 0.1, 0.05)
     intervals = (0, 16, 46, 96, 96)
 
     data = get_data()
-    min_timestamp = min(datum[2] for datum in data)
-    max_timestamp = max(datum[2] for datum in data)
+    data.sort(key=operator.itemgetter(2))
+
+    mindate = min(datum[2] for datum in data)
+    maxdate = max(datum[2] for datum in data)
 
     # The timestamp that is considered as the current one.
-    timestamp = max_timestamp
+    timestamp = maxdate
 
     # Time difference between the min and max timestamps.
-    delta = max_timestamp - min_timestamp
+    delta = maxdate - mindate
+
+    if operation == 'stats':
+        for i, (weight, risk_factor) in enumerate(zip(weights, risk_factors)):
+            print_stats(data, timestamp, i, risk_factor)
+    elif operation == 'graphs':
+        generate_graphs(data, timestamp, mindate, maxdate)
+    else:
+        sys.stderr.write("Invalid argument '%s'.\n" % operation)
+        return 1
 
     print 'datums:', len(data)
-    print 'earliest timestamp:', min_timestamp
-    print 'latest timestamp:', max_timestamp
+    print 'earliest timestamp:', mindate
+    print 'latest timestamp:', maxdate
     print 'delta days:', delta.days
-
-    for i, (weight, risk_factor) in enumerate(zip(weights, risk_factors)):
-        stats(data, timestamp, i, weight, risk_factor)
 
     return 0
 
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv[1:]))
+    args = sys.argv[1:]
+    n = len(args)
+    if n == 1:
+        sys.exit(main(args[0]))
+    else:
+        sys.stderr.write('Not enough arguments.\n')
